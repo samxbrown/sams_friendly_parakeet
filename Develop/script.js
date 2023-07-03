4d2fa3c35c115a215594b136773b90d5c5f7125c
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-07-02 20:44:02 -0700

    updated comments to each function

--- Develop/script.js
+++ Develop/script.js
@@ -15,7 +15,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+// Added the function generate password, then created a while loop so that the user will have to submit the accurate value
+// Added variables to prompt follow-up questions for inclusion of characters
 function generatePassword() {
   var allCharacters = ""
   var passwordLength = parseInt(prompt("Enter the desired length of the password (between 8 and 128 characters):"))
@@ -26,6 +27,8 @@ function generatePassword() {
   var includeNumbers = confirm("Include numeric characters?");
   var includeSpecial = confirm("Include special characters?");
 
+  // Added IF statements to function to validate user selection and if it meets the condition the characters will be added to the pool of all characters
+  // If condition is not met, the characters will not be added to the random password
   if (includeLowercase === true) {
     allCharacters += lowercaseCharacters
   }
@@ -41,11 +44,13 @@ function generatePassword() {
   if (includeSpecial === true) {
     allCharacters += specialCharacers
   }
+
+  // For loop to generate random characters using the Math.floor/Math.random functions until it reaches the password length desired
   var randomPassword = ""
   for (var i = 0; i < passwordLength; i++) {
     randomPassword += allCharacters[Math.floor(Math.random() * allCharacters.length)];
   }
-
+// return generates random password
   return randomPassword
 
 }
